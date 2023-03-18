eca9d14e1b3cc81fad746f58e0e8285ff36aa093
murilonicemento <murilo_nascimento22@outlook.com>
2023-03-18 14:09:21 -0300

    Update Lista de Tarefas

---
+++ lista-de-tarefas/src/components/Main.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "./Main.css";
+
+export default class Main extends Component {
+  state = {
+    novaTarefa: "Lista de Tarefas",
+  };
+  handleChange = (event) => {
+    this.setState({
+      novaTarefa: event.target.value,
+    });
+  };
+  render() {
+    const { novaTarefa } = this.state;
+
+    return (
+      <div className="main">
+        <h1> Lista de Tarefas</h1>
+        <form action="#">
+          <input onChange={this.handleChange} type="text" />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    );
+  }
+}
