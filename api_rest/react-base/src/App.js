f81aed17c9775acb17dddcb7ecb4df6bcb3348be
murilonicemento <murilo_nascimento22@outlook.com>
2023-04-06 17:43:29 -0300

    Inclusão history e criação de rota personzalizada/privada

--- api_rest/react-base/src/App.js
+++ api_rest/react-base/src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
+import { Router } from "react-router-dom";
+import history from "./services/history";
 import Header from "./components/Header";
 import Routes from "./routes";
-import { BrowserRouter } from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Routes />
         <GlobalStyles />
-      </BrowserRouter>
+      </Router>
     </>
   );
 }
