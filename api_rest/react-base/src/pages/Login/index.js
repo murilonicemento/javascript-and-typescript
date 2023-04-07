f2b3c2834146c40e3e04e832fa30bd594b1176c1
murilonicemento <murilo_nascimento22@outlook.com>
2023-04-07 11:41:15 -0300

    Inclusão de toastify e configuração axios

--- api_rest/react-base/src/pages/Login/index.js
+++ api_rest/react-base/src/pages/Login/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { Container } from "../../styles/GlobalStyles";
+import axios from "../../services/axios";
 import { Title, Paragrafo } from "./styled";
 
 export default function Login() {
+    React.useEffect(() => {
+        async function getData() {
+            const response = await axios.get("/alunos");
+            const { data } = response;
+            console.log(data);
+        }
+        getData();
+    }, []);
     return (
         <Container>
             <Title>
