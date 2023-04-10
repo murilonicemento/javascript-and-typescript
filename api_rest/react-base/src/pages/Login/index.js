2b46513e131046470fbee5ef207854b9145231ce
murilonicemento <murilo_nascimento22@outlook.com>
2023-04-10 16:49:53 -0300

    Separando actions e reducers

--- api_rest/react-base/src/pages/Login/index.js
+++ api_rest/react-base/src/pages/Login/index.js
@@ -2,15 +2,17 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { Container } from "../../styles/GlobalStyles";
 import { Title, Paragrafo } from "./styled";
-import * as exampleActions from "../../store/modules/example/actions"
+import * as exampleActions from "../../store/modules/example/actions";
 
 export default function Login() {
     const dispatch = useDispatch();
+
     function handleClick(event) {
         event.preventDefault();
 
-        dispatch(exampleActions);
+        dispatch(exampleActions.clicaBotao());
     }
+
     return (
         <Container>
             <Title>
