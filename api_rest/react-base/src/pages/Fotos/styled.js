3320e00852eb1eb1c4e5227a1c0c1175f8375a10
murilonicemento <murilo_nascimento22@outlook.com>
2023-04-16 11:33:13 -0300

    Editando fotos do aluno

--- api_rest/react-base/src/pages/Fotos/styled.js
+++ api_rest/react-base/src/pages/Fotos/styled.js
@@ -1,5 +1,30 @@
 import styled from "styled-components";
+import * as colors from "../../config/colors";
 
-export const Paragrafo = styled.p`
-    font-size: 90px;
+export const Title = styled.h1`
+    text-align: center;
+`;
+
+export const Form = styled.form`
+    label {
+        width: 180px;
+        height: 180px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: #eee;
+        border: 5px dashed ${colors.primaryColor};
+        border-radius: 50%;
+        margin: 30px auto;
+        cursor: pointer;
+        overflow: hidden;
+
+        img {
+            width: 180px;
+            height: 180px; 
+        }
+    }
+    input {
+        display: none;
+    }
 `;
