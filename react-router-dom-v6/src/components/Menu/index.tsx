caed5f80deed5c8d5af3ddab8e3e0ce064a142cf
murilonicemento <murilo_nascimento22@outlook.com>
2023-04-17 18:31:03 -0300

    Navigate e useNavigate: redirecionamento de páginas

---
+++ react-router-dom-v6/src/components/Menu/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
+
+export const Menu = () => {
+  return (
+    <nav className="menu">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/post">Posts</Link>
+        </li>
+        <li>
+          <Link to="/post/10">Posts 10</Link>
+        </li>
+        <li>
+          <Link to="/redirect">Redirect</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
---
+++ react-router-dom-v6/src/main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import { Menu } from "./components/Menu";
+import { Post } from "./components/Post";
+import { Redirect } from "./components/Redirect";
+import "./styles/global.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/post" element={<Post />} />
+        <Route path="/post/:id" element={<Post />} />
+        <Route path="/redirect" element={<Redirect />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
