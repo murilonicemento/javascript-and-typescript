17a04590f6d1634837a5f6781140513983f795af
murilonicemento <murilo_nascimento22@outlook.com>
2023-05-31 10:02:35 -0300

    Tipos básicos

--- curso-typescript/src/tipos-basicos/type-assertions.ts
+++ curso-typescript/src/tipos-basicos/type-assertions.ts
@@ -1,11 +1,14 @@
-// Condicional
+// Condicional (RECOMENDADO)
 const body = document.querySelector("body");
 if (body) body.style.backgroundColor = "red";
 
-// Non-null assertion (!)
-const body2 = document.querySelector("body")!;
+// Type assertion (RECOMENDADO)
+const body2 = document.querySelector("body") as HTMLBodyElement;
 body2.style.backgroundColor = "red";
-
-// Type assertion
-const body3 = document.querySelector("body") as HTMLBodyElement;
+// Non-null assertion (NÃO RECOMENDADO)
+const body3 = document.querySelector("body")!;
 body3.style.backgroundColor = "red";
+
+// Type assertion (NÃO RECOMENDADO)
+const body4 = document.querySelector("body") as unknown as number;
+body4.style.backgroundColor = "red";
